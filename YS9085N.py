66fd4312742e379e40dc6b19f7d275f496a49425
rldv1 <118821863+rldv1@users.noreply.github.com>
2023-11-10 22:18:48 +0200

    Update YS9085N.py

--- YS9085N.py
+++ YS9085N.py
@@ -8,6 +8,35 @@ if not usb2(d).c_edit(0x1, 1):
     usb2(d).c_reset(0xffffffff)
 # reset if stucks
 
+
+
+#F3 19 C0 FC B2 63 60 07 6A EF B0 70 B7 45 D2 C6 04 4B 37 61 9D FA 56 F7 7F 4D 98 BC 5D CF A8 DB A7 67 F0 0D 48 26
+#00 00 23 24 25 26 27 28 29 2A 2B 2C 00 00 00 00 00 00 00
+NAND = {
+    "parts": {
+        0x00: "silly dumby eblan",
+        0x23: "Micron",
+        0x24: "Intel",
+        0x27: "?", # ~ to 23, maybe Spectek
+        0x34: "Hynix", 
+        0x35: "?", # onfi??? what???
+        0xFF: "silly dumby eblan"
+    },
+
+    "alg": {
+        0x30: "SLC",
+        0x32: "MLC",
+        0x34: "TLC",
+        0x36: "QLC",
+        0x38: "?"
+    }
+
+    
+
+}
+
+
+
 with serial.Serial('/tty/usbserial-14A40', 230400, timeout=10) as s:
     #00 00 00 00 00 00
     #AE
